052b316a3f9d6c1aa12a1e6d2effd2b50020a5c7
kasra <kasra@3C-F8-62-34-0C-88.kasra.com>
2024-12-31 05:04:31 -0500

    added photos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'bg': '#262626',
+      'primary': '#D90A14',
+      'accent': '#D5D5D5',
+    }
     extend: {},
   },
   plugins: [],
